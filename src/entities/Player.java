a65a71656856aaf6ece36c06e15c378e5007630d
GerritScholten <146338174+GerritScholten@users.noreply.github.com>
2024-05-02 19:32:57 +0200

    #8

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,15 +1,13 @@
 package entities;
 
-import javax.imageio.ImageIO;
+import utils.LoadSave;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
 
-import static utils.Constants.Directions.*;
 import static utils.Constants.PlayerConstants.*;
 
-public class Player extends Entity{
+public class Player extends Entity {
     private BufferedImage[][] animations;
 
     private int animationTick, animationSpeed = 15, animationIndex;
@@ -25,131 +23,121 @@ public class Player extends Entity{
         loadAnimations();
     }
 
-    public void update(){
+    public void update() {
         updatePosition();
         updateAnimation();
         setAnimation();
     }
-    public void render(Graphics g){
-        g.drawImage(animations[playerAction][animationIndex], (int)x, (int)y, 256, 256, null);
+
+    public void render(Graphics g) {
+        g.drawImage(animations[playerAction][animationIndex], (int) x, (int) y, 256, 256, null);
 
     }
 
-    private void loadAnimations(){
-        InputStream is = getClass().getResourceAsStream("/DarkSamurai (64x64).png");
-        try {
-            BufferedImage  animationSprite = ImageIO.read(is);
-            animations = new BufferedImage[8][14 ];
-            for (int j = 0; j <= animations.length - 1; j++) {
-                for (int i = 0; i < animations[j].length; i++) {
-                    animations[j][i] = animationSprite.getSubimage(i * 64, j * 64, 64, 64);
-                }
-            }
-        }catch (IOException ex){
-            ex.printStackTrace();
-        }finally {
-            try {
-                is.close();
-            }catch (IOException ex){
-                ex.printStackTrace();
+    private void loadAnimations() {
+        BufferedImage animationSprite = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
+        animations = new BufferedImage[8][14];
+        for (int j = 0; j <= animations.length - 1; j++) {
+            for (int i = 0; i < animations[j].length; i++) {
+                animations[j][i] = animationSprite.getSubimage(i * 64, j * 64, 64, 64);
             }
         }
     }
 
-    private void updateAnimation(){
-        animationTick++;
-        if(animationTick >= animationSpeed){
-            animationTick = 0;
-            animationIndex++;
-            if(animationIndex >= GetSpriteAmount(playerAction)){
-                animationIndex = 0;
-                attacking = false;
-            }
-        }
-    }
-
-
-    private void updatePosition(){
-        isMoving = false;
-
-        if(isLeft() && !isRight()){
-            x -= playerSpeed;
-            isMoving = true;
-        }else if(isRight() && !isLeft()){
-            x += playerSpeed;
-            isMoving = true;
-        }
-
-        if(isUp() && !isDown()){
-            y -= playerSpeed;
-            isMoving = true;
-        }else if(isDown() && !isUp()){
-            y += playerSpeed;
-            isMoving = true;
-        }
-    }
-
-    private void setAnimation() {
-        int startAnimation = playerAction;
-        if (isMoving){
-            playerAction = RUNNING;
-        }else  {
-            playerAction = IDLE;
-        }
-        if (attacking){
-            playerAction = ATTACK_1;
-        }
-        if(playerAction != startAnimation){
-            resetAnimation();
-        }
-    }
-
-    private void resetAnimation() {
-        animationIndex = 0;
+private void updateAnimation() {
+    animationTick++;
+    if (animationTick >= animationSpeed) {
         animationTick = 0;
+        animationIndex++;
+        if (animationIndex >= GetSpriteAmount(playerAction)) {
+            animationIndex = 0;
+            attacking = false;
+        }
+    }
+}
+
+
+private void updatePosition() {
+    isMoving = false;
+
+    if (isLeft() && !isRight()) {
+        x -= playerSpeed;
+        isMoving = true;
+    } else if (isRight() && !isLeft()) {
+        x += playerSpeed;
+        isMoving = true;
     }
 
-    public boolean isLeft() {
-        return left;
+    if (isUp() && !isDown()) {
+        y -= playerSpeed;
+        isMoving = true;
+    } else if (isDown() && !isUp()) {
+        y += playerSpeed;
+        isMoving = true;
     }
+}
 
-    public void setLeft(boolean left) {
-        this.left = left;
+private void setAnimation() {
+    int startAnimation = playerAction;
+    if (isMoving) {
+        playerAction = RUNNING;
+    } else {
+        playerAction = IDLE;
     }
+    if (attacking) {
+        playerAction = ATTACK_1;
+    }
+    if (playerAction != startAnimation) {
+        resetAnimation();
+    }
+}
 
-    public boolean isRight() {
-        return right;
-    }
+private void resetAnimation() {
+    animationIndex = 0;
+    animationTick = 0;
+}
 
-    public void setRight(boolean right) {
-        this.right = right;
-    }
+public boolean isLeft() {
+    return left;
+}
 
-    public boolean isUp() {
-        return up;
-    }
+public void setLeft(boolean left) {
+    this.left = left;
+}
 
-    public void setUp(boolean up) {
-        this.up = up;
-    }
+public boolean isRight() {
+    return right;
+}
 
-    public boolean isDown() {
-        return down;
-    }
+public void setRight(boolean right) {
+    this.right = right;
+}
 
-    public void setDown(boolean down) {
-        this.down = down;
-    }
+public boolean isUp() {
+    return up;
+}
 
-    public void setAttacking(boolean attacking) {
-        this.attacking = attacking;
-    }
+public void setUp(boolean up) {
+    this.up = up;
+}
 
-    public void resetDirectionBooleans() {
-        playerDirection = IDLE;
-        this.setUp(false);
-        this.setLeft(false);
-        this.setRight(false);
-        this.setDown(false);
-    }
+public boolean isDown() {
+    return down;
+}
+
+public void setDown(boolean down) {
+    this.down = down;
+}
+
+public void setAttacking(boolean attacking) {
+    this.attacking = attacking;
+}
+
+public void resetDirectionBooleans() {
+    playerDirection = IDLE;
+    this.setUp(false);
+    this.setLeft(false);
+    this.setRight(false);
+    this.setDown(false);
+}
 }
--- src/utils/LoadSave.java
+++ src/utils/LoadSave.java
@@ -1,9 +1,27 @@
 package utils;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class LoadSave {
-    public static BufferedImage GetPlayerAtlas(){
+    public static final String PLAYER_ATLAS = "DarkSamurai (64x64).png";
 
-    };
+    public static BufferedImage GetSpriteAtlas(String filename) {
+        InputStream is = LoadSave.class.getResourceAsStream("/" + filename);
+        BufferedImage animationSprite = null;
+        try {
+            animationSprite = ImageIO.read(is);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return animationSprite;
+    }
 }
